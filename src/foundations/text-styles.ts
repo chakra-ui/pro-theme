89241ab30ce093d540d32c2157a71cbec79ba330
Christian Schröter <dev@grizzly.codes>
2023-05-24 23:39:57 +0200

    docs: document text styles

--- src/foundations/text-styles.ts
+++ src/foundations/text-styles.ts
@@ -1,12 +1,35 @@
 export default {
-  '7xl': {
-    fontSize: '7xl',
-    lineHeight: '5.75rem', // 92px
-    letterSpacing: '-0.02em',
+  xs: {
+    fontSize: 'xs',
+    lineHeight: '1.125rem', // 18px
   },
-  '6xl': {
-    fontSize: '6xl',
-    lineHeight: '4.5rem', // 72px
+  sm: {
+    fontSize: 'sm',
+    lineHeight: '1.25rem', // 20px
+  },
+  md: {
+    fontSize: 'md',
+    lineHeight: '1.5rem', // 24px
+  },
+  lg: {
+    fontSize: 'lg',
+    lineHeight: '1.75rem', // 28px
+  },
+  xl: {
+    fontSize: 'xl',
+    lineHeight: '1.875rem', // 30px
+  },
+  '2xl': {
+    fontSize: '2xl',
+    lineHeight: '2rem', // 32px
+  },
+  '3xl': {
+    fontSize: '3xl',
+    lineHeight: '2.375rem', // 38px
+  },
+  '4xl': {
+    fontSize: '4xl',
+    lineHeight: '2.75rem', // 44px
     letterSpacing: '-0.02em',
   },
   '5xl': {
@@ -14,37 +37,14 @@ export default {
     lineHeight: '3.75rem', // 60px
     letterSpacing: '-0.02em',
   },
-  '4xl': {
-    fontSize: '4xl',
-    lineHeight: '2.75rem', // 44px
+  '6xl': {
+    fontSize: '6xl',
+    lineHeight: '4.5rem', // 72px
     letterSpacing: '-0.02em',
   },
-  '3xl': {
-    fontSize: '3xl',
-    lineHeight: '2.375rem', // 38px
-  },
-  '2xl': {
-    fontSize: '2xl',
-    lineHeight: '2rem', // 32px
-  },
-  xl: {
-    fontSize: 'xl',
-    lineHeight: '1.875rem', // 30px
-  },
-  lg: {
-    fontSize: 'lg',
-    lineHeight: '1.75rem', // 28px
-  },
-  md: {
-    fontSize: 'md',
-    lineHeight: '1.5rem', // 24px
-  },
-  sm: {
-    fontSize: 'sm',
-    lineHeight: '1.25rem', // 20px
-  },
-  xs: {
-    fontSize: 'xs',
-    lineHeight: '1.125rem', // 18px
+  '7xl': {
+    fontSize: '7xl',
+    lineHeight: '5.75rem', // 92px
+    letterSpacing: '-0.02em',
   },
 }
---
+++ src/stories/text-style.stories.tsx
@@ -0,0 +1,26 @@
+import { SimpleGrid, Text } from '@chakra-ui/react'
+import type { Meta } from '@storybook/react'
+
+const meta: Meta = {
+  title: 'Text Styles',
+}
+
+export default meta
+
+export const Basic = () => {
+  const textStyles = ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl']
+  return (
+    <SimpleGrid columns={2} maxW="2xs" gap="8">
+      {textStyles.map((textStyle) => (
+        <>
+          <Text textStyle="xs" color="muted">
+            {textStyle}
+          </Text>
+          <Text textStyle={textStyle} variant={textStyle}>
+            Ag
+          </Text>
+        </>
+      ))}
+    </SimpleGrid>
+  )
+}
