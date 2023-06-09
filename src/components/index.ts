d1d51e68de1d585c3d7adf459c1fee61d35bb3c3
Christian Schröter <dev@grizzly.codes>
2023-06-09 14:57:01 +0200

    chore: rm close button overrides

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as Badge } from './badge'
 export { default as Button } from './button'
 export { default as Checkbox } from './checkbox'
-export { default as CloseButton } from './close-button'
 export { default as Container } from './container'
 export { default as CustomSelect } from './custom-select'
 export { default as Divider } from './divider'
